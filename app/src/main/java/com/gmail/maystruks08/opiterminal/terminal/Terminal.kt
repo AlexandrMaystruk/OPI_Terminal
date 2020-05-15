4c0a92fc612e0e8b63dbeabc2f58f5f7c884fe43
Alex <maystruks08@gmail.com>
2020-05-15 17:37:55 +0300

    Start communication with terminal

--- app/src/main/java/com/gmail/maystruks08/opiterminal/terminal/Terminal.kt
+++ app/src/main/java/com/gmail/maystruks08/opiterminal/terminal/Terminal.kt
@@ -5,15 +5,16 @@ import com.gmail.maystruks08.opiterminal.entity.request.*
 import java.io.DataInputStream
 import java.io.DataOutputStream
 import java.io.IOException
-import java.net.InetAddress
-import java.net.ServerSocket
-import java.net.Socket
-import java.net.SocketException
-import java.nio.charset.Charset
+import java.net.*
 import java.util.*
 
 
-class Terminal(private val ipAddress: String, private val inputPort: Int, private val outputPort: Int, private val timeout: Int?) {
+class Terminal(
+    private val ipAddress: String?,
+    private val inputPort: Int?,
+    private val outputPort: Int?,
+    private val timeout: Int?
+) {
 
     private val tag = "Terminal"
     private val socketConnectionHelper = SocketConnectionHelper()
@@ -31,31 +32,32 @@ class Terminal(private val ipAddress: String, private val inputPort: Int, privat
         fun timeout(timeout: Int) = apply { this.timeout = timeout }
 
         fun build(): Terminal {
-            if (ipAddress == null || inputPort == null) throw Exception() //TODO change exception type
             return Terminal(
-                ipAddress!!,
-                inputPort!!,
-                outputPort!!,
+                ipAddress,
+                inputPort,
+                outputPort,
                 timeout
             )
         }
     }
 
     fun login() {
+        socketConnectionHelper.openSendConnection(ipAddress, inputPort)?.let {
 
-        val socket = socketConnectionHelper.openSendConnection(ipAddress, inputPort, timeout)
+            val serviceRequest = ServiceRequest(requestType = "Login", applicationSender = "SmartCheckout", workstationID = getLocalIpAddress(), requestID = UUID.randomUUID().toString())
 
-        if (socket != null) {
-            socketConnectionHelper.sendData("<?xml version=\"1.0\" encoding=\"utf-8\"?> <ServiceRequest xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" RequestType=\"Login\" ApplicationSender=\"CashAssist\" WorkstationID=\"WINDOWS-3N69TMJ\" RequestID=\"192\" />".toUTF8())
+            serviceRequest.serializeToXMLString()?.let {
+                socketConnectionHelper.sendData(it)
+            }
 
-            socketConnectionHelper.receiveData(ipAddress, outputPort) {
+            socketConnectionHelper.receiveData(outputPort) {
                 Log.d(tag, "Receive login data $it")
             }
         }
     }
 
     fun transaction(paymentData: Payment) {
-        socketConnectionHelper.receiveData(ipAddress, outputPort) {
+        socketConnectionHelper.receiveData(outputPort) {
             Log.d(tag, "Receive transaction data $it")
         }
 
@@ -74,7 +76,7 @@ class Terminal(private val ipAddress: String, private val inputPort: Int, privat
     }
 
     fun logout() {
-        socketConnectionHelper.receiveData(ipAddress, outputPort) {
+        socketConnectionHelper.receiveData(outputPort) {
             Log.d(tag, "Receive logout data $it")
         }
         socketConnectionHelper.sendData("xml")
@@ -82,32 +84,22 @@ class Terminal(private val ipAddress: String, private val inputPort: Int, privat
     }
 }
 
-
-private fun String.toUTF8(): String{
-    return this.toByteArray().toString(Charset.defaultCharset())
-}
-
-
 class SocketConnectionHelper {
 
     private val tag = "SocketConnectionHelper"
     private var sendDataSocket: Socket? = null
-    private var receiveDataSocket: Socket? = null
+    private var serverSocket: ServerSocket? = null
+    private var clientSocket: Socket? = null
     private var operationTime = 0L
-    private var defaultTimeout = 10000
-
-    // used to send messages
     private var bufferOut: DataOutputStream? = null
-
-    // used to read messages from the server
     private var bufferIn: DataInputStream? = null
 
-    fun openSendConnection(serverIpAddress: String, serverPort: Int, timeout: Int?): Socket? {
+    fun openSendConnection(ipAddress: String?, port: Int?): Socket? {
         val beginTicks: Long = System.currentTimeMillis()
         Log.d(tag, "Open connection started")
 
         // Check IP and Port not empty
-        if (serverIpAddress.isEmpty() || serverPort == 0) {
+        if (ipAddress.isNullOrEmpty() || port == null || port == 0) {
             operationTime = System.currentTimeMillis() - beginTicks
             Log.d(tag, "Error serverIpAddress or serverPort is empty.")
             Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
@@ -122,21 +114,18 @@ class SocketConnectionHelper {
             return null
         }
 
-        // Create and connect socket
         return try {
-            val serverAddress: InetAddress = InetAddress.getByName(serverIpAddress)
+            val address: InetAddress = InetAddress.getByName(ipAddress)
             Log.d(tag, "Connecting..")
-
             //create a socket to make the connection with the server
-            sendDataSocket = Socket(serverAddress, serverPort).apply { soTimeout = timeout?: defaultTimeout }
-            Log.d(tag, "Socket created")
+            sendDataSocket = Socket(address, port)
             if (sendDataSocket?.isConnected == true) {
-                Log.d(tag, "Socket connected $serverIpAddress $serverPort")
+                Log.d(tag, "Socket connected $ipAddress : $port")
                 operationTime = System.currentTimeMillis() - beginTicks
                 Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
                 sendDataSocket
             } else {
-                Log.d(tag, "Connect socket error $serverIpAddress $serverPort")
+                Log.d(tag, "Connect socket error $ipAddress : $port")
                 operationTime = System.currentTimeMillis() - beginTicks
                 Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
                 null
@@ -154,92 +143,43 @@ class SocketConnectionHelper {
         }
     }
 
-   private fun openReceiveConnection(serverPort: Int) {
-        val beginTicks: Long = System.currentTimeMillis()
-        Log.d(tag, "Open connection started")
-
-        // Check IP and Port not empty
-        if ( serverPort == 0) {
-            operationTime = System.currentTimeMillis() - beginTicks
-            Log.d(tag, "Error serverIpAddress or serverPort is empty.")
-            Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
-            return
-        }
-
-        // Check that socket not exists
-        if (receiveDataSocket != null) {
-            operationTime = System.currentTimeMillis() - beginTicks
-            Log.d(tag, "Error socket alreadyExists.")
-            Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
-            return
-        }
-
-        // Create and connect socket
-         try {
-            Log.d(tag, "Connecting..")
-
-            val serverSocket = ServerSocket(serverPort)
-
-            while (true){
-                //create a socket to make the connection with the server
-                receiveDataSocket = serverSocket.accept()
-
-                Log.d(tag, "Socket created")
-                if (receiveDataSocket?.isConnected == true) {
-                    operationTime = System.currentTimeMillis() - beginTicks
-                    Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
-                } else {
-                    Log.d(tag, "Connect socket error $serverPort")
-                    operationTime = System.currentTimeMillis() - beginTicks
-                    Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
-                }
-            }
-        } catch (socketException: SocketException) {
-            operationTime = System.currentTimeMillis() - beginTicks
-            Log.d(tag, "Socket exception while connecting $socketException")
-            Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
-        } catch (e: Exception) {
-            operationTime = System.currentTimeMillis() - beginTicks
-            Log.d(tag, "Unknown exception while connecting $e")
-            Log.d(tag, "Method openConnection() finished. Operation time ${Date(operationTime)}")
-        }
-    }
-
-    @Throws(Exception::class)
     fun sendData(data: String) {
-        if (sendDataSocket == null || sendDataSocket?.isClosed == true) {
-            throw Exception("Send data error. Socket not created or closed")
-        }
         try {
-            //sends the message to the server
             bufferOut = DataOutputStream(sendDataSocket!!.getOutputStream())
             bufferOut?.writeUTF(data)
             bufferOut?.flush()
+            Log.d(tag, "Send data $data")
         } catch (e: IOException) {
-            throw Exception("Send data error: " + e.message)
+            bufferIn?.close()
+            Log.d(tag, "Send data error: " + e.message)
         }
     }
 
-    @Throws(Exception::class)
-    fun receiveData(serverIpAddress: String, serverPort: Int, onDataReceived: (String) -> Unit) {
-
-        openReceiveConnection(serverPort)
-
-        if (receiveDataSocket == null || receiveDataSocket?.isClosed == true) {
-            throw Exception("Receive data error. Socket not created or closed")
-        }
+    fun receiveData(receivePort: Int?, onDataReceived: (String) -> Unit) {
         try {
-            //receives the message which the server sends back
-            bufferIn = DataInputStream(receiveDataSocket!!.getInputStream());
-            //in this while the client listens for the messages sent by the server
-            while (receiveDataSocket?.isConnected == true) {
-                val serverMessage = bufferIn?.readUTF()
-                if (serverMessage != null ) {
-                    onDataReceived(serverMessage)
+            val beginTicks: Long = System.currentTimeMillis()
+            if(receivePort == null || receivePort == 0){
+                operationTime = System.currentTimeMillis() - beginTicks
+                Log.d(tag, "Error receivePort is null or 0.")
+                Log.d(tag, "Method receiveData() finished. Operation time ${Date(operationTime)}")
+                return
+            }
+            val deviceIp = getLocalIpAddress()
+            serverSocket = ServerSocket(receivePort, 0, InetAddress.getByName(deviceIp))
+            Log.d(tag, " Waiting for incoming connection on IP $deviceIp and port $receivePort")
+            while (true) {
+                serverSocket?.accept()?.use { socket ->
+                    clientSocket = socket
+                    DataInputStream(socket.getInputStream()).use {
+                        while (socket.isConnected) {
+                            val serverMessage = it.readUTF()
+                            onDataReceived(serverMessage)
+                        }
+                    }
                 }
             }
-        } catch (e: IOException) {
-            throw Exception("Receive data error: $e")
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
@@ -255,10 +195,14 @@ class SocketConnectionHelper {
         }
         return try {
             sendDataSocket?.close()
+            clientSocket?.close()
+            serverSocket?.close()
             bufferOut?.flush()
             bufferOut?.close()
             bufferIn?.close()
             sendDataSocket = null
+            serverSocket = null
+            clientSocket = null
             bufferOut = null
             bufferIn = null
 
@@ -278,4 +222,24 @@ class SocketConnectionHelper {
             false
         }
     }
+}
+
+fun getLocalIpAddress(): String {
+    return try {
+        val networkInterfaces = NetworkInterface.getNetworkInterfaces()
+        while (networkInterfaces.hasMoreElements()) {
+            val networkInterface = networkInterfaces.nextElement()
+            val enumIpAddress = networkInterface.inetAddresses
+            while (enumIpAddress.hasMoreElements()) {
+                val internetAddress = enumIpAddress.nextElement()
+                if (!internetAddress.isLoopbackAddress && internetAddress is Inet4Address) {
+                    return internetAddress.getHostAddress()
+                }
+            }
+        }
+        ""
+    } catch (ex: SocketException) {
+        ""
+    }
+
 }
